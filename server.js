886743d8a12233f0ed0df8aa43b7fa6e6542f307
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-08-28 21:43:32 +0900

    fix author API

--- server.js
+++ server.js
@@ -1,47 +1,171 @@
 console.log("web serverni boshlash");
+
 const express = require("express");
 const app = express();
 const http = require("http");
 const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) =>{
-if(err) {
-  console.log("ERROR", err);
-} else {
-    user = JSON.parse(data)
-  }
-});
+let user = {};
+let authors = [];
 
-// 1: Kirish code
+// --------------------
+// Read user.json
+// --------------------
+try {
+  const userData = fs.readFileSync("database/user.json", "utf8");
+  user = JSON.parse(userData);
+} catch (err) {
+  console.log("ERROR user.json:", err.message);
+}
+
+// --------------------
+// Read authors.json
+// --------------------
+try {
+  const authorsData = fs.readFileSync("database/authors.json", "utf8");
+  authors = JSON.parse(authorsData);
+} catch (err) {
+  console.log("ERROR authors.json:", err.message);
+  authors = [];
+}
+
+// --------------------
+// Save authors
+// --------------------
+function saveAuthors(res, successData, status = 200) {
+  fs.writeFile(
+    "database/authors.json",
+    JSON.stringify(authors, null, 2),
+    (err) => {
+      if (err) {
+        console.log("ERROR:", err);
+        return res.status(500).json({
+          error: "Fayl yozishda xatolik"
+        });
+      }
+
+      res.status(status).json(successData);
+    }
+  );
+}
+
+// --------------------
+// Middleware
+// --------------------
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-// 2: session code
-// 3: views code
+// --------------------
+// EJS
+// --------------------
 app.set("views", "views");
 app.set("view engine", "ejs");
 
+// --------------------
+// Author API
+// --------------------
 
-// 4: Routing code
-app.post("/create-item", (req, res) => {
-  // to do: code with db here
+// Get all authors
+app.get("/api/authors", (req, res) => {
+  res.json(authors);
 });
+
+// Get one author
+app.get("/api/authors/:id", (req, res) => {
+  const author = authors.find(
+    (a) => a.id == req.params.id
+  );
+
+  if (!author) {
+    return res.status(404).json({
+      error: "Author topilmadi"
+    });
+  }
+
+  res.json(author);
+});
+
+// Create author
+app.post("/api/authors", (req, res) => {
+  const newAuthor = {
+    id: Date.now(),
+    ...req.body
+  };
+
+  authors.push(newAuthor);
+
+  saveAuthors(res, newAuthor, 201);
+});
+
+// Update author
+app.put("/api/authors/:id", (req, res) => {
+  const author = authors.find(
+    (a) => a.id == req.params.id
+  );
+
+  if (!author) {
+    return res.status(404).json({
+      error: "Author topilmadi"
+    });
+  }
+
+  Object.assign(author, req.body);
+
+  saveAuthors(res, author);
+});
+
+// Delete author
+app.delete("/api/authors/:id", (req, res) => {
+  const exists = authors.some(
+    (a) => a.id == req.params.id
+  );
+
+  if (!exists) {
+    return res.status(404).json({
+      error: "Author topilmadi"
+    });
+  }
+
+  authors = authors.filter(
+    (a) => a.id != req.params.id
+  );
+
+  saveAuthors(res, {
+    message: "Author o'chirildi"
+  });
+});
+
+// --------------------
+// Author WEB PAGE
+// --------------------
 
 app.get("/author", (req, res) => {
-res.render("author",{ user: user });
-})
-
-
-app.get("/", function (req, res) {
-    res.send("LOCALHOST IS WORKING!");
+  res.render("author", {
+    user: user,
+    authors: authors
+  });
 });
+
+// --------------------
+// Home
+// --------------------
+
+app.get("/", (req, res) => {
+  res.send("LOCALHOST IS WORKING!");
+});
+
+// --------------------
+// Start server
+// --------------------
 
 const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function(){
-console.log(`The server is running 
-  succesfully on port: ${PORT}`);
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(
+    `The server is running successfully on port: ${PORT}`
+  );
 });
+ 
