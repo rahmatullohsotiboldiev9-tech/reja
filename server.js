ef9ff48d09ca484620116bc43bb1e7905948d731
rahmatullohsotiboldiev9-tech <rahmatullohsotiboldiev9@gmail.com>
2026-08-30 17:25:56 +0900

    train.js

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+
+/*// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+// 3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing code
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+  console.log(`The server is running successfully on port: ${PORT}`);
+});
+*/
+
+
+
+
+
+
+
+
+
 console.log("web serverni boshlash");
 
 const express = require("express");
